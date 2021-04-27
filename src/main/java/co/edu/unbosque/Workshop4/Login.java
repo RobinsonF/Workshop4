d70c38c66c2e09ac0678060e65b813f173db726e
RobinsonF <rgutierrezs@unbosque.edu.co>
2021-04-27 17:44:32 -0500

    Pagina de inicio

--- src/main/java/co/edu/unbosque/Workshop4/Login.java
+++ src/main/java/co/edu/unbosque/Workshop4/Login.java
@@ -1,2 +1,15 @@
-package co.edu.unbosque.Workshop4;public class Login {
+package co.edu.unbosque.Workshop4;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "login", value = "/login")
+public class Login extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("type/jsp");
+    String nombre = request.getParameter("Nombre");
+    }
 }
