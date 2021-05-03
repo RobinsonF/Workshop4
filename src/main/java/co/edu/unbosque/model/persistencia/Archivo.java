a218c1e2b4321ae6705d6fa6c3c64268537ae4f9
Camilo Gomez <fcgomez@unbosque.edu.co>
2021-05-03 15:09:06 -0500

    Creando la clase archivo

---
+++ src/main/java/co/edu/unbosque/model/persistencia/Archivo.java
@@ -0,0 +1,39 @@
+package co.edu.unbosque.model.persistencia;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Archivo {
+    private final String SAMPLE_CSV_FILE = "Data\\pets.csv";
+
+    public Archivo() {
+
+    }
+
+    public void escribirArchivo(ArrayList<Usuario> lista) {
+        System.out.println("Entro al escribir");
+        String DELIMITADOR = ",";
+        String SALTO = "\n";
+        FileWriter bw;
+        try {
+            bw = new FileWriter(SAMPLE_CSV_FILE);
+
+            for (Usuario pet : lista) {
+                bw.append(SALTO);
+                bw.append(pet.getNombre());
+                bw.append(DELIMITADOR);
+                bw.append(pet.getDescripcion());
+                bw.append(DELIMITADOR);
+                bw.append(String.valueOf(pet.getFecha()));
+                bw.append(DELIMITADOR);
+                bw.append(String.valueOf(pet.getFoto()));
+            }
+            bw.flush();
+            bw.close();
+        } catch (IOException e) {
+            System.out.println(e);
+
+        }
+    }
+
+}
