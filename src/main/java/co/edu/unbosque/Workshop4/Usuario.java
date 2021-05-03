6fc572d4a1c123ac6151b2c91af0ddf717027946
Camilo Gomez <fcgomez@unbosque.edu.co>
2021-05-03 15:13:35 -0500

    Terminando la clase usuario, con sus getters and setters

--- src/main/java/co/edu/unbosque/Workshop4/Usuario.java
+++ src/main/java/co/edu/unbosque/Workshop4/Usuario.java
@@ -22,4 +22,36 @@ public class Usuario {
         this.fecha = fecha;
         this.foto = foto;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public InputStream getFoto() {
+        return foto;
+    }
+
+    public void setFoto(InputStream foto) {
+        this.foto = foto;
+    }
 }
