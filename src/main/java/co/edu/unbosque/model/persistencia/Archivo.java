b00492c918a0f898d18ce90e72a0c7241191086b
Camilo Gomez <fcgomez@unbosque.edu.co>
2021-05-03 15:16:02 -0500

    Creando el método de leer archivo

--- src/main/java/co/edu/unbosque/model/persistencia/Archivo.java
+++ src/main/java/co/edu/unbosque/model/persistencia/Archivo.java
@@ -2,9 +2,11 @@ package co.edu.unbosque.model.persistencia;
 
 import co.edu.unbosque.Workshop4.Usuario;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Archivo {
     private final String SAMPLE_CSV_FILE = "Data\\pets.csv";
@@ -38,5 +40,50 @@ public class Archivo {
 
         }
     }
+    public ArrayList<Usuario> leerArchivo() {
+        ArrayList<Usuario> lista = new ArrayList<Usuario>();
+        BufferedReader bufferLectura = null;
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        Date fechaDate = null;
+        try {
+            bufferLectura = new BufferedReader(new FileReader(SAMPLE_CSV_FILE));
+            String linea = bufferLectura.readLine();
 
+            while (linea != null) {
+                Usuario pet = new Usuario();
+                String[] campos = linea.split(",");
+                if (campos.length == 4) {
+                    try {
+                        pet.setNombre(campos[0]);
+                        pet.setDescripcion(campos[1]);
+                        pet.setFecha(formato.parse(campos[2]));
+                        pet.setFoto(new ByteArrayInputStream(campos[3].getBytes()));
+                        lista.add(pet);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Error al leer archivo number");
+
+                    } catch (ParseException e) {
+                        System.out.println("Error al leer archivo parse");
+
+                        e.printStackTrace();
+                    }
+                }
+                linea = bufferLectura.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error al leer archivo");
+        } finally {
+            if (bufferLectura != null) {
+                try {
+                    bufferLectura.close();
+                } catch (IOException e) {
+                    System.out.println("Error al leer archivo");
+
+                    e.printStackTrace();
+                }
+            }
+        }
+        return lista;
+    }
 }
